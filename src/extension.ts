ceec4bd52209c014b83381f7e57432a409090ba6
ashton_sun <sunqiyuan@live.com>
2025-01-17 21:29:01 +0800

    feat(ui): activity bar 增加入口

--- src/extension.ts
+++ src/extension.ts
@@ -13,13 +13,34 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('codetrack.helloWorld', () => {
+	const disposableTest = vscode.commands.registerCommand('codetrack.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from codetrack!');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposableTest);
+
+    // 添加命令以打开 Webview 面板
+    let disposable = vscode.commands.registerCommand('codetrack.openView', async () => {
+        const panel = vscode.window.createWebviewPanel(
+            'codetrackView',
+            'CodeTrack',
+            vscode.ViewColumn.One,
+            {}
+        );
+
+        // 加载 HTML 内容
+        panel.webview.html = await getWebviewContent(context);
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+async function getWebviewContent(context: vscode.ExtensionContext): Promise<string> {
+    const htmlPath = vscode.Uri.file(context.asAbsolutePath('src/panel.html'));
+    const htmlContent = await vscode.workspace.fs.readFile(htmlPath);
+    return htmlContent.toString();
 }
 
 // This method is called when your extension is deactivated
